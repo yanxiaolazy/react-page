b1ad0a348c103d24508f13a8a7b7ae097bf207cd
is-rookie <1977735808@qq.com>
2020-09-21 16:18:57 +0800

    update

--- serve/routes/index.js
+++ serve/routes/index.js
@@ -1,11 +1,8 @@
 const router = require('@koa/router')();
 const server = require('./server/renderHTML');
-const { resolve } = require('path');
 const renderConfig = require('./server/renderConfig');
 
 module.exports = router;
-
-const root = resolve('./');
 
 router.get(/./, server());
 
